74da740143900208f09d93f7ae1e01d20a8db87c
Nicknameee <igrunka1991@gmail.com>
2023-12-23 20:51:57 +0200

    Init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false;
+
+new Vue({
+    render: h => h(App)
+}).$mount('body');
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module: {
+    rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+            }
+        }
+    ]
+}
